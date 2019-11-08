9e07bdd6cdb721d6f2be66677c7ca363cf00c7b5
Michael Calkins <m.calkins576@gmail.com>
2019-11-08 15:59:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# Add your code here
+class Dog 
+  attr_accessor :name 
+  
+  
+  @@all = []
+  
+  def initialize
+    @@all << self  
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.print_all
+    
+  
+end
